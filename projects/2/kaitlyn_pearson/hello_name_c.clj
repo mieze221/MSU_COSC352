eed08546dfb5fb06eb0b96aa3694245d15e49c5c
Kaitlyn Pearson <167239611+kapea8@users.noreply.github.com>
2025-02-27 09:01:19 -0500

    Update hello_name_c.clj

--- projects/2/kaitlyn_pearson/hello_name_c.clj
+++ projects/2/kaitlyn_pearson/hello_name_c.clj
@@ -1,19 +1,5 @@
 
-;; (ns hello-name-c
-;;   (:gen-class))
-
-;; (defn -main
-;;   [& args]
-;;   (if (= (count args) 2)
-;;     (let [name (first args)
-;;           count (Integer/parseInt (second args))]
-;;       (dotimes [_ count]
-;;         (println (str "Hello, " name "!"))))
-;;     (println "Usage: hello-name-c <name> <count>" args)))
-
-;; (-main)
-
-(def args *command-line-args*)  ;; Get arguments
+(def args *command-line-args*)
  (println "Arguments received:" args)
 
  (if (= (count args) 2)
