0809bee52a1cad39d16414730b61c9f7fe3c62d6
Jaylon Williams <jawil316@morgan.edu>
2025-03-11 17:16:36 -0400

    Update hello.jl

--- projects/2/jaylon_williams/hello.jl
+++ projects/2/jaylon_williams/hello.jl
@@ -1,19 +1,22 @@
 function main()
-    
+
     if length(ARGS) != 2
-    println("Usage: julia hello.jl <name> <number>")
-    exit(1)
+        println("Usage: julia hello.jl <name> <number>")
+        exit(1)
+    end
+
+    name = ARGS[1]
+    num = try
+        parse(Int, ARGS[2])
+    catch
+        println("Error: <number> must be an integer")
+        exit(1)
+    end
+
+    for i in 1:num
+        println("Hello ", name)
+    end
 end
 
-name = ARGS[1]
-num = try
-    parse(Int, ARGS[2])
-catch
-    println("Error: Second argument must be an integer.")
-    exit(1)
-end
+main()
 
-# Print the greeting multiple times
-for _ in 1:num
-    println("Hello $name")
-end
