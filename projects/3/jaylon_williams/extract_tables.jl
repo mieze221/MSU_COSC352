6ecbf6b3c47919b5215a64eee54501894badc3b6
Jaylon Williams <jawil316@morgan.edu>
2025-03-13 19:12:13 -0400

    Update extract_tables.jl

--- projects/3/jaylon_williams/extract_tables.jl
+++ projects/3/jaylon_williams/extract_tables.jl
@@ -12,16 +12,25 @@ tables = []
 table_start = "<table"
 table_end = "</table>"
 
-start_idx = findnext(table_start, html_content, 1)
-while start_idx !== nothing
-    end_idx = findnext(table_end, html_content, start_idx + length(table_start) - 1)
+global start_pos = 1
+while true
+    global start_idx = findnext(table_start, html_content, start_pos)
+    if start_idx === nothing
+        break
+    end
+    
+    start_int = first(start_idx)
+    
+    end_idx = findnext(table_end, html_content, start_int + length(table_start))
     if end_idx === nothing
         break
     end
-
-    push!(tables, html_content[start_idx:end_idx + length(table_end) - 1])
-
-    start_idx = findnext(table_start, html_content, end_idx + length(table_end) - 1)
+    
+    end_int = last(end_idx) + length(table_end) - 1
+    
+    push!(tables, html_content[start_int:end_int])
+    
+    global start_pos = end_int + 1
 end
 
 # Step 4: Save extracted tables into CSV files
