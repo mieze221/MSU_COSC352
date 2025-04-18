5607e8ec09af0ab2aae4c6caf2f67d52b027e4ec
compsage <jonwhite@jons-mbp.mynetworksettings.com>
2025-04-18 08:21:49 -0400

    here we go

--- projects/5/jon_white/hello-world.js
+++ projects/5/jon_white/hello-world.js
@@ -30,3 +30,14 @@ export const wasmBrowserInstantiate = async (wasmModuleUrl, importObject) => {
 
   return response;
 };
+const runWasmAdd = async () => {
+  // Instantiate our wasm module
+  const wasmModule = await wasmBrowserInstantiate("./hello-world.wasm");
+
+  // Call the Add function export from wasm, save the result
+  const addResult = wasmModule.instance.exports.add(24, 24);
+
+  // Set the result onto the body
+  document.body.textContent = `Hello World! addResult: ${addResult}`;
+};
+runWasmAdd();
