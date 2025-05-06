2cf3248f8c05b8f7844fe829ac56940135f5cd13
mieze221 <mieze4@morgan.edu>
2025-05-06 13:48:34 -0400

    Delete projects/5/Michael_Ezeigbo/src directory

--- projects/5/Michael_Ezeigbo/src/lib.rs
+++
@@ -1,8 +0,0 @@
-use wasm_bindgen::prelude::*;
-
-/// Generate a repeated greeting message.
-#[wasm_bindgen]
-pub fn generate_greeting(name: &str, age: u32, repeat: usize) -> String {
-    let message = format!("Hello, {}. You are {} years old.\n", name, age);
-    message.repeat(repeat)
-}
